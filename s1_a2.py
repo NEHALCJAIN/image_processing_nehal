0cab33ed603781f4741d0ed9acf2fddb6a02c8af
NEHALCJAIN <nehal.cj@somaiya.edu>
2020-06-06 18:10:41 +0530

    flips

---
+++ s1_a2.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy
+
+cap = cv2.VideoCapture(0)
+count = 0
+while True:
+    count = count + 1
+    x,red= cap.read()
+    vertf1 = numpy.rot90(red)
+    if (count % 2 == 0):
+        cv2.imshow("img",vertf1)
+    else:
+        vertf2=cv2.flip(vertf1,-1)
+        cv2.imshow("img",vertf2)
+    if cv2.waitKey(1000)&0xFF==ord('q'):
+        break
