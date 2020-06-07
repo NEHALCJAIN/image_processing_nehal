81c05af4b3f5db2a279d2538843ab344a9e458c0
Nehal Jain <46996711+NEHALCJAIN@users.noreply.github.com>
2020-06-07 14:22:34 +0530

    Add files via upload

---
+++ s2_a3.py
@@ -0,0 +1,14 @@
+import cv2
+import time
+cam=cv2.VideoCapture(0)
+start=time.time()
+end=time.time()
+while True:
+    x,red=cv2.read()
+    if end-start==5:
+        flipped=cv2.flip(red,-1)
+        cv2.imshow('win',flipped)
+    else:
+        cv2.imshow('win',red)
+    if cv2.waitKey(1)&0xFF==ord('q'):
+        break
